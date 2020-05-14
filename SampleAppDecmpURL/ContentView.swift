9dfe8378dcbdf3949f2863ffd90548a49f187fc2
gucchisk <gucchi_sk@yahoo.co.jp>
2020-05-14 22:32:43 +0900

    Initial Commit

---
+++ SampleAppDecmpURL/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SampleAppDecmpURL
+//
+//  Created by Satoshi Kobayashi on 2020/05/14.
+//  Copyright © 2020 Satoshi Kobayashi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
